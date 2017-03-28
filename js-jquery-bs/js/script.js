413cea7b2277b68670130d82b80bb580b2fed15f
Gabriel Martinez B <gabriel@gcs-gabriel.greencore.int>
2017-03-27 23:30:53 -0600

    funcion anonima

--- js-jquery-bs/js/script.js
+++ js-jquery-bs/js/script.js
@@ -1,3 +1,16 @@
-var mostrarResultados = function(texto, estilo){
-	$("#mostrarResultado").val(texto);
+var mostrarResultados = function(texto,estilo){
+    $("#mostrarResultado").val(texto);
+	$("#mostrarResultado").css("background-color",estilo);
+}
+
+
+$(document).ready(function(){
+  $("#boton-anonima").click(function(){
+    mostrarResultados('Anonima','red');
+  });
+});
+
+function mytest(){
+	$('#mostrarResultado').val('prueba');
+	alert("Hello");
 }
