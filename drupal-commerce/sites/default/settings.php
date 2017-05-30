fd05b7b97b29ba98ab37e3dda4126eaaf66f25b6
root <root@lamp-1gb-nyc2-01.localdomain>
2017-05-30 20:36:07 +0000

    Commerce funcionando en el servidor

--- drupal-commerce/sites/default/settings.php
+++ drupal-commerce/sites/default/settings.php
@@ -764,9 +764,9 @@ $settings['file_scan_ignore_directories'] = [
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'drupal_commerce',
-  'username' => 'drupaluser',
-  'password' => 'drupalpassword',
+  'database' => 'dp_114520031',
+  'username' => '114520031',
+  'password' => 'drupal114520031',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
